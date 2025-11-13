95b46b0970e52afc414f48b0aabdafa93dab7c96
shopia1122 <shopia123@sookmyung.ac.kr>
2025-11-13 15:44:57 +0900

    main6

--- main.c
+++ main.c
@@ -4,22 +4,18 @@
 
 int main(int argc, char *argv[])
 {
-     int i;
-     int grade[5];
-     int sum=0;
-
-     for(i=0;i<5;i++) {
-     printf("input value[%i] = ", i);
-     scanf("%d", &grade[i]);
-      }
+     int i=100;
+     int *p = &i;
+     int **q = &p;
+     
+     *p = 200;
+     printf("i = %d, *p = %d, **q = %d\n", i, *p, **q);
+     
+      **q = 300;
+     printf("i = %d, *p = %d, **q = %d\n", i, *p, **q);
     
-    for(i=0;i<5;i++) 
-    {
-       printf("grade[%i] = %i\n", i, *(grade + i));
-       sum = sum + *(grade + i);            
-    }
     
-    printf("average: %i\n", sum/5);
+   
   system("PAUSE");	
   return 0;
 }
